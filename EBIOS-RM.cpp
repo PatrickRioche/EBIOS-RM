260135214b0f0498cee7a8745c3d400972cae5fa
PatrickR <patrick.rioche@gmail.com>
2024-01-11 10:55:24 +0100

    Ajoutez des fichiers projet.

---
+++ EBIOS-RM.cpp
@@ -0,0 +1,12 @@
+﻿// EBIOS-RM.cpp : définit le point d'entrée de l'application.
+//
+
+#include "EBIOS-RM.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
